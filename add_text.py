2845022cdead0efe4d819fa3a7b7e1c5cd0067ca
aydansmith <aydanhsmith@gmail.com>
2022-03-30 21:45:01 -0500

    got initial guess squares added

---
+++ add_text.py
@@ -0,0 +1,18 @@
+import pygame
+import sys
+
+# rgb colors
+BLACK = (0, 0, 0)
+WHITE = (200, 200, 200)
+
+# font text is from https://www.geeksforgeeks.org/python-display-text-to-pygame-window/
+# learned how to do text in pygame and based code off of https://stackoverflow.com/questions/10467863/how-to-remove-replace-text-in-pygame
+# adds text to top of screen
+def add_text(screen, text):
+    # fills the screen black in the area wanted so that previous text doesn't show up still
+    screen.fill(BLACK, (0, 0, 600, 90))
+    font = pygame.font.Font('freesansbold.ttf', 50)
+    text = font.render(text, True, WHITE)
+    textRect = text.get_rect()
+    textRect.center = (300, 50)
+    screen.blit(text, textRect)
---
+++ main.py
@@ -0,0 +1,173 @@
+import pygame
+import sys
+import add_text
+#colors in RGB form
+
+BLACK = (0, 0, 0)
+WHITE = (200, 200, 200)
+YELLOW = (255, 196, 37)
+GREEN = (50, 205, 50)
+
+# height of window for pygame
+WINDOW_HEIGHT = 600
+#width of window for pygame
+WINDOW_WIDTH = 600
+#initializes screen in pygame
+SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+CAPTION = pygame.display.set_caption("Wordle")
+
+
+ROW_1 = []
+ROW_2 = []
+ROW_3 = []
+ROW_4 = []
+ROW_5 = []
+ROW_6 = []
+
+ALPHABET_ORDER = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'BACK']
+ALPHABET = []
+
+# main handles all the logic and passing between files
+def main():
+
+    # following code is inspired and similar to thread on creating a grid for a snake game in pygane
+    # https://stackoverflow.com/questions/33963361/how-to-make-a-grid-in-pygame
+    global SCREEN, CLOCK
+    # initializes pygame
+    pygame.init()
+    # creates clock in pygame
+    CLOCK = pygame.time.Clock()
+    #fill screen to black
+    SCREEN.fill(BLACK)
+    add_text.add_text(SCREEN, "Wordle")
+    createBoxesForGuesses()
+    while True:
+        pos = pygame.mouse.get_pos()
+        printBoxesForGuesses()
+        pygame.display.update()
+        for event in pygame.event.get():
+                # if the user wants to quit, close pygame
+                # if the user clicks, we respond accordingly
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+
+
+def createBoxesForGuesses():
+    fillRowOne()
+    fillRowTwo()
+    fillRowThree()
+    fillRowFour()
+    fillRowFive()
+    fillRowSix
+
+def printBoxesForGuesses():
+    printRowOne()
+    printRowTwo()
+    printRowThree()
+    printRowFour()
+    printRowFive()
+    printRowSix()
+
+def fillRowOne():
+    blockSize = 40 # set blcok size
+    x = 180 # initial x
+    y = 100 # constant y
+    # we will add 4 blocks
+    for count in range(0,5):
+        rect = pygame.Rect(x, y, blockSize, blockSize) # create rectangle 
+        ROW_1.append(rect) # add rectangle to row
+        x = x + 50 # update x
+
+def fillRowTwo():
+    blockSize = 40 # set blcok size
+    x = 180 # initial x
+    y = 150 # constant y
+    # we will add 4 blocks
+    for count in range(0,5):
+        rect = pygame.Rect(x, y, blockSize, blockSize) # create rectangle 
+        ROW_2.append(rect) # add rectangle to row
+        x = x + 50 # update x
+
+def fillRowThree():
+    blockSize = 40 # set blcok size
+    x = 180 # initial x
+    y = 200 # constant y
+    # we will add 4 blocks
+    for count in range(0,5):
+        rect = pygame.Rect(x, y, blockSize, blockSize) # create rectangle 
+        ROW_3.append(rect) # add rectangle to row
+        x = x + 50 # update x
+
+def fillRowFour():
+    blockSize = 40 # set blcok size
+    x = 180 # initial x
+    y = 250 # constant y
+    # we will add 4 blocks
+    for count in range(0,5):
+        rect = pygame.Rect(x, y, blockSize, blockSize) # create rectangle 
+        ROW_4.append(rect) # add rectangle to row
+        x = x + 50 # update x
+
+def fillRowFive():
+    blockSize = 40 # set blcok size
+    x = 180 # initial x
+    y = 300 # constant y
+    # we will add 4 blocks
+    for count in range(0,5):
+        rect = pygame.Rect(x, y, blockSize, blockSize) # create rectangle 
+        ROW_5.append(rect) # add rectangle to row
+        x = x + 50 # update x
+
+def fillRowSix():
+    blockSize = 40 # set blcok size
+    x = 180 # initial x
+    y = 350 # constant y
+    # we will add 4 blocks
+    for count in range(0,5):
+        rect = pygame.Rect(x, y, blockSize, blockSize) # create rectangle 
+        ROW_6.append(rect) # add rectangle to row
+        x = x + 50 # update x
+
+def printRowOne():
+    # prints each rectangle in row array
+    for x in ROW_1:
+        pygame.draw.rect(SCREEN, WHITE, x, 1)
+
+def printRowTwo():
+    # prints each rectangle in row array
+    for x in ROW_2:
+        pygame.draw.rect(SCREEN, WHITE, x, 1)
+
+def printRowThree():
+    # prints each rectangle in row array
+    for x in ROW_3:
+        pygame.draw.rect(SCREEN, WHITE, x, 1)
+
+def printRowFour():
+    # prints each rectangle in row array
+    for x in ROW_4:
+        pygame.draw.rect(SCREEN, WHITE, x, 1)
+
+def printRowFive():
+    # prints each rectangle in row array
+    for x in ROW_5:
+        pygame.draw.rect(SCREEN, WHITE, x, 1)
+
+def printRowSix():
+    # prints each rectangle in row array
+    for x in ROW_6:
+        pygame.draw.rect(SCREEN, WHITE, x, 1)
+
+def fillAlphabet():
+    blockWidth = 30 
+    blockHeight = 40
+    x = 105
+    y = 420
+    
+
+
+# so that each import does not call main function
+if __name__ == "__main__":
+    main()
